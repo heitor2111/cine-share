1e683ad9c10d2ca04ceacb45c4d81816767bbacd
Heitor Henrique <heitor2111@gmail.com>
2025-11-08 15:34:30 +0000

    correção no husky

--- scripts/post-create.sh
+++ scripts/post-create.sh
@@ -15,3 +15,8 @@ fi
 
 # instala dependências básicas
 bun install
+
+# configura o pre-commit hook com o PATH correto do bun
+if [ -f .husky/pre-commit ]; then
+  sed -i '2a\\n# Adiciona o caminho do bun ao PATH\nexport PATH="/home/node/.bun/bin:$PATH"\n' .husky/pre-commit
+fi
